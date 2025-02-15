1f3ee10482ff9843dca4d973891cb2ea12f8ff43
ThangNguyen <thangnh.pb@gmail.com>
2025-02-15 08:09:01 +0700

    tạo project và giao diện cho MainActivity

---
+++ app/src/main/java/com/example/baitap1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.baitap1;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtA,edtB,edtC;
+    AppCompatButton btnTinh,btnWeb;
+    TextView ketqua;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtC = findViewById(R.id.edtC);
+        btnTinh = findViewById(R.id.bnTinh);
+        btnWeb = findViewById(R.id.bnWeb);
+        ketqua = findViewById(R.id.ketqua);
+
+
+    }
+}
